c05215fb22b1fe4aecfd0ed8dc1d50456d877207
praveen.hemadri <praveen.hemadri@user-265.otsi.com>
2020-01-23 14:44:20 +0530

    Added exeption handling methods

--- src/main/java/com/otsi/springbootdemo/exception/ExceptionHandler.java
+++
@@ -1,9 +0,0 @@
-package com.otsi.springbootdemo.exception;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.zalando.problem.spring.web.advice.ProblemHandling;
-
-@ControllerAdvice
-public class ExceptionHandler implements ProblemHandling {
-
-}
